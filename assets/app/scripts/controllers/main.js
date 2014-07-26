aff46b7dc73b7dd04c5ab518ff69a8fbafbcf8bb
Jassiem Moore <jassiemmoore@gmail.com>
2014-07-26 14:34:34 -0400

    Changed picture size, fonts, etc...

--- assets/app/scripts/controllers/main.js
+++ assets/app/scripts/controllers/main.js
@@ -4,34 +4,5 @@
 function MainCtrl($scope, apiService, $http, cordovaReady, $timeout) {
   $scope.output = '';
 
-  $scope.init = function() {
-  	apiService.get(function(data,status){
-	  	if( status === 200) {
-	  		$scope.output = data;
-	  	} else {
-	  		$scope.output = 'error with api call';
-	  	}
-	  	
-	  },'/api/test', {});
-
-	  document.addEventListener('deviceready', $scope.startGCM(), false);
-  };
-
-  $scope.startGCM = function() {
-  	var successHandler = function(result) {
-  		//alert("Success. Result = " + result);
-      console.log('success');
-  	};
-
-  	var errorHandler = function(error) {
-  		alert('giraffes');
-  	};
-
-		var pushNotification = window.plugins.pushNotification;
-		pushNotification.register(successHandler, errorHandler,{"senderID":"198488259531","ecb":"app.onNotification"});
-  };
-
-  $scope.init();
-
 }
-MainCtrl.$inject = ['$scope', 'apiService', '$http', 'cordovaReady', '$timeout'];
+MainCtrl.$inject = ['$scope', 'apiService', '$http', '$timeout'];
